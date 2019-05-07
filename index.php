6a16d3742c0990ef1aa9103fb12d99f8f71bee0c
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-05-07 13:57:37 +0700

    membuat index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Unbeatable tic tac toe</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Unbeatable tic tac toe</h1>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-6">
+      <form>
+        <div class="form-group">
+          <label>Log</label>
+          <textarea class="form-control" id="log" rows="15"></textarea>
+      </div>
+      </form>
+  </div>
+</div>
+
+</body>
+</html>
